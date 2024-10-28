8dd3f128243ad0890c50cad381cd12dcd95a3e26
Asma Maria <asantaa31@gmail.com>
2024-10-28 11:03:58 +0600

    my commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    //await client.connect();
     const database = client.db('booksbucket_db_user');
     const userCollection = database.collection('users');
     const productCollection = database.collection('products');
@@ -132,8 +132,8 @@ async function run() {
         res.send(result)
     })
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to Books Bucket Database!");
+    //await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to Books Bucket Database!");
   } finally {
 
   }
@@ -142,7 +142,7 @@ async function run() {
 run().catch(console.dir);
 
 //import json data
-const featured = require("./data/featured.json")
+
 const faq = require("./data/faq.json")
 
 //APIs
@@ -150,9 +150,6 @@ app.get('/', (req, res)=>{
     res.send("Books Bucket Server is running")
 })
 
-app.get('/featured', (req, res)=>{
-    res.send(featured);
-})
 
 app.get('/faq', (req, res)=>{
     res.send(faq);
